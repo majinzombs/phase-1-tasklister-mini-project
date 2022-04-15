5f15c0df0d7138cfe3d9704977c0983355496945
Jonathan Koban <jon.koban@outlook.com>
2022-04-14 20:31:29 -0400

    Challenge: TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// Grab all the DOM
+
+// Form stuff and inputs
+const newTaskForm = document.getElementById('create-task-form');
+const newTaskDescription = document.getElementById('new-task-description');
+const newTask = document.getElementById('new-task');
+
+
+// ul where the new task will live
+const newTaskUl = document.getElementById('tasks');
+
+
+// attach event Listeners
+newTaskForm.addEventListener('submit', createNewTask);
+
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  //Stop form from trying to submit
+
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+  };
+
